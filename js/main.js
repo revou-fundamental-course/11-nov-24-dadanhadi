fe4d0bffd8fd49fe21941d517448a14b5592b09f
dadanhadi <dadanhadi9@gmail.com>
2024-11-19 21:45:12 +0700

    penambahan auto slide

--- js/main.js
+++ js/main.js
@@ -1,1 +1,35 @@
-console.log("halo")
+const carouselWrapper = document.querySelector('.carousel-wrapper');
+const listPhotos = document.querySelectorAll('.carousel-photo');
+const prevButton = document.querySelector('.arrow-prev');
+const nextButton = document.querySelector('.arrow-next');
+
+    let currentIndex = 0;
+
+const showSlide = (index) => {
+    const totalSlides = listPhotos.length;
+    currentIndex = (index + totalSlides) % totalSlides;
+    carouselWrapper.style.transform = `translateX(-${currentIndex * 100}%)`;
+};
+
+prevButton.addEventListener('click', () => {
+    showSlide(currentIndex - 1);
+});
+
+nextButton.addEventListener('click', () => {
+    showSlide(currentIndex + 1);
+});
+
+let autoSlide = setInterval(() => {
+    showSlide(currentIndex + 1);
+}, 5000);
+
+carouselWrapper.addEventListener("mouseenter", () => {
+    clearInterval(autoSlide);
+});
+
+carouselWrapper.addEventListener("mouseleave", () => {
+    autoSlide = setInterval(() => {
+        showSlide(currentIndex + 1);
+    }, 5000);
+});
+
